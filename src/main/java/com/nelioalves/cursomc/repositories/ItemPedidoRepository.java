62727ac49b67eac0c394839533801a85237b7b4a
plinioaraujo <plinioglad@gmail.com>
2018-04-10 06:30:43 -0300

    Endpoint /pedidos/{id} e atualizando o JsonIgnore

--- src/main/java/com/nelioalves/cursomc/repositories/ItemPedidoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ItemPedidoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.cursomc.domain.ItemPedido;
-import com.nelioalves.cursomc.domain.Pedido;
 
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	public Pedido buscar(Integer id){
+		 Optional<Pedido> cli = pedidoRepository.findById(id);
+		 
+		 String msg = "Objeto não encontrado! ";
+		 return cli.orElseThrow( () -> new ObjectNotFoundException(msg + id + ", Tipo: " + Pedido.class.getName() ) );
+	}
+	
+}
