8ec5f054d8188fccb212a6e78da90c01b5305b2c
plinioaraujo <plinioglad@gmail.com>
2018-04-20 23:43:13 -0300

    Validacao customizada email nao repetido na insercao de Cliente

--- src/main/java/com/nelioalves/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/nelioalves/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,11 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 import com.nelioalves.cursomc.dto.ClienteNewDTO;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
 import com.nelioalves.cursomc.resources.exception.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -18,15 +26,21 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getId()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
-			list.add(new FieldMessage("cpfOuCnpj","CPF inválido!"));
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getId()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido!"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
-			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido!"));
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
 		}
-		
+
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
