cc21231ed68d8e84f4922ac7bbaa358de55d811e
plinioaraujo <plinioglad@gmail.com>
2018-09-06 07:40:30 -0300

    Restricao de conteudo: Cliente so recupera ele mesmo

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
-import com.nelioalves.cursomc.domain.Pedido;
+
 
 public class MockEmailService extends AbstractEmailService {
 
