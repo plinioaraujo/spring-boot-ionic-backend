2f487df9f6cdd7651aa1c290bf93dcae508b1b40
plinioaraujo <plinioglad@gmail.com>
2018-09-07 09:13:25 -0300

    Esqueci a senha

---
+++ src/main/java/com/nelioalves/cursomc/dto/EmailDTO.java
@@ -0,0 +1,28 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty(message="O email é obrigatório")
+	@Email(message="Email inválido")
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -78,5 +79,22 @@ public abstract class AbstractEmailService implements EmailService {
 		
 		return mimeMessage;
 	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass){
+		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(cliente,newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha ");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova Senha : " + newPass);
+		return sm;
+	}
+	
 
 }
