c0b618aab3689d9be4e1a24b197b386d08c64344
plinioaraujo <plinioglad@gmail.com>
2018-09-13 07:48:25 -0300

    ajustando tamanho da imagem

--- src/main/java/com/nelioalves/cursomc/services/ImageService.java
+++ src/main/java/com/nelioalves/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,45 +19,53 @@ import com.nelioalves.cursomc.services.exceptions.FileException;
 @Service
 public class ImageService {
 
-	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile){
+	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
 		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename());
-		if (!"png".equals(ext) && (!"jpg".equals(ext))){
+		if (!"png".equals(ext) && (!"jpg".equals(ext))) {
 			throw new FileException("Somente imagens PNG ou JPG são permitidas");
 		}
-		
+
 		try {
 			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
-			
-			if("png".equals(img)){
+
+			if ("png".equals(img)) {
 				img = pngtoJpg(img);
 			}
-			
+
 			return img;
-			
+
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler o arquivo");
 		}
-		
+
 	}
 
 	public BufferedImage pngtoJpg(BufferedImage img) {
 		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-		jpgImage.createGraphics().drawImage(img,0, 0, Color.WHITE,null);
+		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
 		return jpgImage;
 	}
-	
-	
-	public InputStream getInputStream(BufferedImage img, String extension){
-	
+
+	public InputStream getInputStream(BufferedImage img, String extension) {
+
 		try {
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
-			
+
 			ImageIO.write(img, extension, os);
 			return new ByteArrayInputStream(os.toByteArray());
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler o arquivo");
 		}
-	  
 
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(sourceImg, (sourceImg.getWidth() / 2) - (min / 2), (sourceImg.getHeight() / 2) - (min / 2),
+				min, min);
+	}
+
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
